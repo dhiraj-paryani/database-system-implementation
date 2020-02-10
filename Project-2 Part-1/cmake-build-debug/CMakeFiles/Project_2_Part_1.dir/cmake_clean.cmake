98603f4cc850a55c19009589015c508cfc39bc97
Dhiraj Paryani <dhiraj.mahesh.paryani@gmail.com>
2020-02-10 14:46:31 -0500

    Project-2 Part-1 Base code

---
+++ Project-2 Part-1/cmake-build-debug/CMakeFiles/Project_2_Part_1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_2_Part_1.dir/Comparison.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/ComparisonEngine.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/DBFile.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/DBFileGTests.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/File.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/Record.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/Schema.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/TwoWayList.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/gtest/gtest-all.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/gtest/gtest_main.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/main.cc.o"
+  "CMakeFiles/Project_2_Part_1.dir/test.cc.o"
+  "Project_2_Part_1"
+  "Project_2_Part_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_2_Part_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Project-2 Part-1/cmake-build-debug/CMakeFiles/Project_2_Part_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Project_2_Part_1.
+# This may be replaced when dependencies are built.
