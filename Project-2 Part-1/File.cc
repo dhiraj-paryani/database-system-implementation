539e0bc33fc2a7d0576c0cd064c9e8e8b40a3808
Dhiraj Paryani <dhiraj.mahesh.paryani@gmail.com>
2020-02-10 15:22:46 -0500

    Part 1 changes

--- Project-2 Part-1/File.cc
+++ Project-2 Part-1/File.cc
@@ -1,13 +1,13 @@
 #include "File.h"
 #include "TwoWayList.cc"
 
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <iostream>
+#include <stdlib.h>
 
 
 
@@ -110,7 +110,6 @@ void Page :: FromBinary (char *bits) {
 
 	// first read the number of records on the page
 	numRecs = ((int *) bits)[0];
-	//subi //cerr << " numRecs in page " << numRecs << endl;
 
 	// sanity check
 	if (numRecs > 1000000 || numRecs < 0) {
@@ -168,7 +167,6 @@ void File :: GetPage (Page *putItHere, off_t whichPage) {
 
 	// this is because the first page has no data
 	whichPage++;
-	//subi// cerr << "get_pg " << whichPage << " file_sz " << curLength << endl;
 
 	if (whichPage >= curLength) {
 		cerr << "whichPage " << whichPage << " length " << curLength << endl;
