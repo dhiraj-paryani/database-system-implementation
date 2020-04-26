1ba93c4efa8925fcdc3724f06b33f6212af41291
Dhiraj <dhiraj.mahesh.paryani@gmail.com>
2020-04-25 21:51:24 -0400

    "Project file driver"

---
+++ Project/test.cc
@@ -0,0 +1,31 @@
+#include "Database.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Database database;
+
+    database.TurnOn();
+
+    cout << "Database Turned Up Successfully\n";
+
+    int input = 0;
+    while(input != 2) {
+        cout << " select option: \n";
+        cout << " \t 1. Execute a query \n";
+        cout << " \t 2. Exit \n";
+        cin >> input;
+        if (input == 1) {
+            cout << "Enter Query: (when done press ctrl-D):\n";
+
+            database.ExecuteQuery();
+        }
+        input = 2;
+    }
+
+    database.ShutDown();
+
+    cout << "Database Shut Down Successfully\n";
+}
